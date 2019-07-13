692d57fb09b0536aadcc0ae0a24888be9600779a
Kevin Grant <ConsciousK@yahoo.com>
2019-07-13 12:15:21 -0400

    Created Redux Actions.

---
+++ todo/src/actions/index.js
@@ -0,0 +1,24 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const GET_TODOS = 'GET_TODOS';
+
+export const addTodo = todo => {
+  return {
+    type: ADD_TODO,
+    payload: todo
+  };
+};
+
+export const toggleTodo = id => {
+  return {
+    type: TOGGLE_TODO,
+    payload: id
+  };
+};
+
+export const getTodos = todos => {
+  return {
+    type: GET_TODOS,
+    payload: todos
+  };
+};
