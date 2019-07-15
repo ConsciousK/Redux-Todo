bb5ca788ba6074e276a2a42fc2be763f26eb7ba3
Kevin Grant <ConsciousK@yahoo.com>
2019-07-15 18:22:34 -0400

    MVP met

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../App.css';
+import './App.css';
 import TodoListContainer from './TodoListContainer';
 
 class App extends Component {
--- todo/src/index.js
+++ todo/src/index.js
@@ -7,7 +7,7 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 
 const store = createStore(
-  rootReducer,
+  rootReducer
 );
 
 ReactDOM.render(
